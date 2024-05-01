c264fac218ef97a580638c0836259996b915054a
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2024-05-01 23:04:17 +0200

    tree choping

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,36 +3,37 @@ extends CharacterBody2D
 const SPEED = 120
 var player_state = "idle"
 var player_is_attacking = false
-var attack_cooldown = false
 signal player_attacking
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction.x == 0 && direction.y == 0:
 		player_state = "idle"
-		if Input.is_action_just_pressed("attack") && !player_is_attacking && !attack_cooldown:
-			player_is_attacking = true
-			$AnimatedSprite2D.play("attacking")
-			attack_cooldown = true
-			await get_tree().create_timer(1 - 0.7).timeout
-			player_attacking.emit()
-			print("hit !")
-			await get_tree().create_timer(1 - 0.7).timeout
-			player_is_attacking = false
-			attack_cooldown = false
-			$AnimatedSprite2D.play("idle")
+		if Input.is_action_just_pressed("attack") && !player_is_attacking:
+			attack()
 	else:
 		player_state = "walking"
+	
 	velocity = direction * SPEED
-	play_animation(direction, player_state)
 	if !player_is_attacking:
+		play_animation(direction, player_state)
 		move_and_slide()
+
+func attack():
+	player_is_attacking = true
+	$AnimatedSprite2D.play("attacking")
+	await get_tree().create_timer(1 - 0.7).timeout
+	player_attacking.emit()
+	print("hit !")
+	await get_tree().create_timer(1 - 0.7).timeout
+	player_is_attacking = false
+	$AnimatedSprite2D.play("idle")
 
 func play_animation(direction: Vector2, player_state: String):
 	if player_state == "idle":
 		if !player_is_attacking:
 			$AnimatedSprite2D.play("idle")
-	elif player_state == "walking" && !player_is_attacking:
+	elif player_state == "walking":
 		$AnimatedSprite2D.play("walking")
 	
 	if direction.x < 0:
--- scripts/tree.gd
+++ scripts/tree.gd
@@ -3,15 +3,26 @@ extends StaticBody2D
 var tree_health = 3
 var is_area_entered = false
 
-func _process(delta):
-	if is_area_entered:
-		if Input.is_action_just_pressed("attack"):
-			$AnimatedSprite2D.play("hit")
-
 func _on_chop_area_body_entered(body):
-	if body.has_method() == "player":
+	if body.name == "player":
 		is_area_entered = true
 
 func _on_chop_area_body_exited(body):
-	if body.has_method() == "player":
+	if body.name == "player":
 		is_area_entered = false
+
+func _on_player_player_attacking():
+	if is_area_entered:
+		if tree_health > 0:
+			tree_health -= 1
+			if tree_health == 0:
+				$AnimatedSprite2D.play("choped")
+				$growth_timer.start()
+			else:
+				$AnimatedSprite2D.play("hit")
+				await get_tree().create_timer(0.5).timeout
+				$AnimatedSprite2D.play("idle")
+
+func _on_growth_timer_timeout():
+	tree_health = 3
+	$AnimatedSprite2D.play("idle")
