5a5069c10a267a4d0371f960fb7a446187ffd68d
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2024-05-01 22:24:37 +0200

    player can attack

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,30 +3,39 @@ extends CharacterBody2D
 const SPEED = 120
 var player_state = "idle"
 var player_is_attacking = false
+var attack_cooldown = false
 signal player_attacking
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction.x == 0 && direction.y == 0:
 		player_state = "idle"
-		if Input.is_action_just_pressed("attack"):
+		if Input.is_action_just_pressed("attack") && !player_is_attacking && !attack_cooldown:
 			player_is_attacking = true
-			await get_tree().create_timer(1 - 4 * 1/10).timeout
+			$AnimatedSprite2D.play("attacking")
+			attack_cooldown = true
+			await get_tree().create_timer(1 - 0.7).timeout
+			player_attacking.emit()
+			print("hit !")
+			await get_tree().create_timer(1 - 0.7).timeout
 			player_is_attacking = false
+			attack_cooldown = false
+			$AnimatedSprite2D.play("idle")
 	else:
 		player_state = "walking"
 	velocity = direction * SPEED
-	move_and_slide()
-	play_animation(direction, player_state, player_is_attacking)
+	play_animation(direction, player_state)
+	if !player_is_attacking:
+		move_and_slide()
 
-func play_animation(direction: Vector2, player_state: String, player_is_attacking: bool):
+func play_animation(direction: Vector2, player_state: String):
 	if player_state == "idle":
-		if player_is_attacking:
-			$AnimatedSprite2D.play("attacking")
-			await get_tree().create_timer(1 - 4 * 1/10).timeout
-			player_attacking.emit()
-			print("hit !")
-		else:
+		if !player_is_attacking:
 			$AnimatedSprite2D.play("idle")
-	if player_state == "walking":
+	elif player_state == "walking" && !player_is_attacking:
 		$AnimatedSprite2D.play("walking")
+	
+	if direction.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	elif direction.x > 0:
+		$AnimatedSprite2D.flip_h = false
