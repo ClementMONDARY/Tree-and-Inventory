55a2fe66e643963bfcf21192759d28097dc7921c
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2024-10-23 21:50:36 +0200

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,47 +2,46 @@ extends Control
 
 signal game_started
 signal menu_opened
-
 var is_game_started = false
-var buttons_container: VBoxContainer = null
-var fade_in_anim: Animation = null
-var fade_out_anim: Animation = null
-var hover_sound: AudioStreamPlayer = null
 
 func _ready():
-	buttons_container = get_node("Buttons_VBoxContainer")
-	fade_in_anim = buttons_container.AnimationPlayer.get_animation("fade_in")
-	fade_out_anim = buttons_container.AnimationPlayer.get_animation("fade_out")
-	hover_sound = get_node("Buttons_VBoxContainer/Hover_Buttons")
+	pass
 
 func _on_play_button_pressed():
-	buttons_container.AnimationPlayer.play(fade_out_anim)
-	await(fade_out_anim.get_length())
-	buttons_container.visible = false
+	end_music($main_music)
+	$Buttons_VBoxContainer/AnimationPlayer.play("fade_out")
+	await get_tree().create_timer($Buttons_VBoxContainer/AnimationPlayer.get_animation("fade_out").get_length()).timeout
+	$Buttons_VBoxContainer.visible = false
 	is_game_started = true
 	game_started.emit()
 
 func _on_settings_button_pressed():
-	pass
+	pass # Replace with function body.
 
 func _on_exit_button_pressed():
-	buttons_container.ExitGame.play()
+	$Buttons_VBoxContainer/ExitGame.play()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("escape") && is_game_started:
 		is_game_started = false
 		menu_opened.emit()
-		buttons_container.AnimationPlayer.play(fade_in_anim)
-		buttons_container.visible = true
-		await(fade_in_anim.get_length())
+		$Buttons_VBoxContainer/AnimationPlayer.play("fade_in")
+		$Buttons_VBoxContainer.visible = true
+		await get_tree().create_timer($Buttons_VBoxContainer/AnimationPlayer.get_animation("fade_in").get_length()).timeout
 
-# Removed unnecessary end_music function
+func end_music(music: AudioStreamPlayer) -> void:
+	var anim_player = music.get_node("AnimationPlayer")
+	anim_player.play("fade_in")
+	await get_tree().create_timer(anim_player.get_animation("fade_in").get_length()).timeout
+	music.stream_paused = true
 
-func _on_play_button_mouse_entered():
-	hover_sound.play()
+# Sounds
 
-func _on_settings_button_mouse_entered():
-	hover_sound.play()
+func _on_play_button_mouse_entered() -> void:
+	$Buttons_VBoxContainer/Hover_Buttons.play()
 
-func _on_exit_button_mouse_entered():
-	hover_sound.play()
+func _on_settings_button_mouse_entered() -> void:
+	$Buttons_VBoxContainer/Hover_Buttons.play()
+
+func _on_exit_button_mouse_entered() -> void:
+	$Buttons_VBoxContainer/Hover_Buttons.play()
