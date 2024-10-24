096550920bce532e908b0a38541ecdeab76e323a
clement.mondary <clement.mondary@edu.ecole-89.com>
2024-10-24 14:19:46 +0200

    player_camera: script: dynamic camera zoom for both open and close menu

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -1,11 +1,21 @@
 extends Camera2D
 
-var is_game_started = false
-var is_camera_detached = false
+enum MenuAction {
+	Open,
+	Close,
+}
+
+const ATTACHED_ZOOM = Vector2(2, 2)
+const DETACHED_ZOOM = Vector2(1.75, 1.75)
+
+var is_game_started: bool
+var is_camera_detached: bool
 
 func _ready() -> void:
 	set_zoom(Vector2(1.5, 1.5))
 	$Blackscreen/AnimationPlayer.play("fade_out")
+	is_game_started = false
+	is_camera_detached = false
 
 func _on_exit_button_pressed() -> void:
 	$Blackscreen/AnimationPlayer.play("fade_in")
@@ -20,7 +30,7 @@ func _process(_delta: float) -> void:
 			false:
 				detach_anim()
 	if Input.is_action_just_pressed("escape") && is_game_started:
-		open_menu()
+		menu_anim(MenuAction.Open)
 
 func change_camera_mode():
 	match is_camera_detached:
@@ -29,10 +39,29 @@ func change_camera_mode():
 		false:
 			detach_anim()
 
-func open_menu():
+func menu_anim(action: MenuAction):
+	match action:
+		MenuAction.Open:
+			open_menu_anim()
+		MenuAction.Close:
+			close_menu_anim()
+
+func open_menu_anim():
 	$AnimationPlayer.get_animation("open_menu").track_set_key_value(0, 0, zoom)
 	$AnimationPlayer.play("open_menu")
 	is_game_started = false
+
+func close_menu_anim():
+	$AnimationPlayer.get_animation("close_menu").track_set_key_value(0, 0, zoom)
+	match is_camera_detached:
+		true:
+			$AnimationPlayer.get_animation("close_menu").track_set_key_value(0, 1, DETACHED_ZOOM)
+		false:
+			$AnimationPlayer.get_animation("close_menu").track_set_key_value(0, 1, ATTACHED_ZOOM)
+	print($AnimationPlayer.get_animation("close_menu").track_get_key_value(0, 0))
+	print($AnimationPlayer.get_animation("close_menu").track_get_key_value(0, 1))
+	$AnimationPlayer.play("close_menu")
+	is_game_started = true
 
 func attach_anim():
 	$AnimationPlayer.play("attach_to_player_zoom")
@@ -48,12 +77,4 @@ func detach_anim():
 
 func _on_play_button_pressed() -> void:
 	if !is_game_started:
-		match is_camera_detached:
-			true:
-				$AnimationPlayer.play("zoom_in_detached")
-				await get_tree().create_timer($AnimationPlayer.get_animation("zoom_in_detached").get_length()).timeout
-				is_game_started = true
-			false:
-				$AnimationPlayer.play("zoom_in_attached")
-				await get_tree().create_timer($AnimationPlayer.get_animation("zoom_in_attached").get_length()).timeout
-				is_game_started = true
+		menu_anim(MenuAction.Close)
