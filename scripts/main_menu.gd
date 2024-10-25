f706f2c7ce4b36d745a02fec209236530e5d9508
clement.mondary <clement.mondary@edu.ecole-89.com>
2024-10-25 12:24:27 +0200

    next step: free camera with phantom camera addon

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,11 +7,6 @@ var is_game_started = false
 func _ready():
 	pass
 
-func _on_play_button_pressed():
-	end_music($main_music)
-	is_game_started = true
-	game_started.emit()
-
 func _on_settings_button_pressed():
 	pass # Replace with function body.
 
@@ -20,10 +15,17 @@ func _process(delta: float) -> void:
 		is_game_started = false
 		menu_opened.emit()
 
-func end_music(music: AudioStreamPlayer) -> void:
-	var anim_player = music.get_node("AnimationPlayer")
-	anim_player.play("fade_in")
-	await get_tree().create_timer(anim_player.get_animation("fade_in").get_length()).timeout
-	music.stream_paused = true
+func end_main_music() -> void:
+	$main_music/AnimationPlayer.get_animation("fade_in").track_set_key_value(0, 0, $main_music.volume_db)
+	$main_music/AnimationPlayer.play("fade_in")
 
 # signals ------------------------------------------------------------------------------------------
+
+func _on_play_button_pressed():
+	end_main_music()
+	is_game_started = true
+	game_started.emit()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		$main_music.stop()
