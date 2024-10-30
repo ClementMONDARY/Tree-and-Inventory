adf4c1af0f0f0f3f40675dd0bfd3990d020be666
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2024-10-30 19:06:12 +0100

    pointer icon for freecam outside of player range

--- scripts/free_cam.gd
+++ scripts/free_cam.gd
@@ -2,7 +2,10 @@ extends PhantomCamera2D
 
 var is_dragging := false
 var drag_start_pos := Vector2()
-@onready var map_limit_target: CollisionShape2D = get_node("/root/world/MapDelimiter")
+
+@onready var player = get_node("/root/world/game_nodes/player")
+@onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
+@onready var icon = path_follow.get_child(0)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -11,20 +14,21 @@ func _input(event):
 			drag_start_pos = event.position
 		else:
 			is_dragging = false
-	
 	elif event is InputEventMouseMotion and is_dragging:
 		var delta_motion = (event.position - drag_start_pos) / zoom
 		position -= delta_motion
 		drag_start_pos = event.position
-		apply_camera_limits()
 
-func apply_camera_limits():
-	var shape = map_limit_target.shape
-	if shape is RectangleShape2D:
-		var min_x = map_limit_target.global_position.x - shape.extents.x
-		var max_x = map_limit_target.global_position.x + shape.extents.x
-		var min_y = map_limit_target.global_position.y - shape.extents.y
-		var max_y = map_limit_target.global_position.y + shape.extents.y
-		
-		position.x = clamp(position.x, min_x, max_x)
-		position.y = clamp(position.y, min_y, max_y)
+func _process(delta: float) -> void:
+	update_direction_icon()
+
+func update_direction_icon():
+	var screen_size = get_viewport_rect().size * 0.35
+	var relative_position = to_local(player.global_position)
+	if (abs(relative_position.x) > screen_size.x or abs(relative_position.y) > screen_size.y) && priority == 1:
+		icon.visible = true
+		var angle = global_position.angle_to_point(player.global_position)
+		angle += PI
+		path_follow.progress_ratio = fposmod(angle, TAU) / TAU
+	else:
+		icon.visible = false
