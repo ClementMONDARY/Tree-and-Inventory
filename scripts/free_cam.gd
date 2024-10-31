6fcfc1ec4b62f553a011dc690d078782fb8add96
clement.mondary <clement.mondary@edu.ecole-89.com>
2024-10-31 11:55:46 +0100

    icon pointer

--- scripts/free_cam.gd
+++ scripts/free_cam.gd
@@ -5,7 +5,7 @@ var drag_start_pos := Vector2()
 
 @onready var player = get_node("/root/world/game_nodes/player")
 @onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
-@onready var icon = path_follow.get_child(0)
+@onready var pointer_icon := $Path2D/PathFollow2D/Pointer
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -20,15 +20,30 @@ func _input(event):
 		drag_start_pos = event.position
 
 func _process(delta: float) -> void:
-	update_direction_icon()
+	update_pointer_icon()
 
-func update_direction_icon():
-	var screen_size = get_viewport_rect().size * 0.35
+func update_pointer_icon():
+	var screen_size = get_viewport_rect().size / 2  # Demi-tailles de l'écran pour le centrage
 	var relative_position = to_local(player.global_position)
-	if (abs(relative_position.x) > screen_size.x or abs(relative_position.y) > screen_size.y) && priority == 1:
-		icon.visible = true
+	
+	if abs(relative_position.x) > screen_size.x or abs(relative_position.y) > screen_size.y:
+		path_follow.visible = true
+		
+		# Calcul de l'angle pour pointer vers le joueur
 		var angle = global_position.angle_to_point(player.global_position)
-		angle += PI
-		path_follow.progress_ratio = fposmod(angle, TAU) / TAU
+		
+		# Conversion de l'angle en position sur les bords du rectangle de l'écran
+		var max_x = screen_size.x * cos(angle)
+		var max_y = screen_size.y * sin(angle)
+		
+		# Ajustement pour que l'icône se place sur les bords de l'écran
+		if abs(max_x) > screen_size.x:
+			max_x = sign(max_x) * screen_size.x
+		if abs(max_y) > screen_size.y:
+			max_y = sign(max_y) * screen_size.y
+		
+		# Positionnement de l'icône en fonction des coordonnées calculées
+		path_follow.offset = Vector2(max_x, max_y).length()
+		pointer_icon.rotation = angle
 	else:
-		icon.visible = false
+		path_follow.visible = false
