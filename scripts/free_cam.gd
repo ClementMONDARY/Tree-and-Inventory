460943ea7a8ab65b126ba5c9ef47a290a633b6f1
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2024-11-07 22:29:22 +0100

    freecam now only manage cursor dragging, not more pointer icon

--- scripts/free_cam.gd
+++ scripts/free_cam.gd
@@ -4,8 +4,6 @@ var is_dragging := false
 var drag_start_pos := Vector2()
 
 @onready var player = get_node("/root/world/game_nodes/player")
-@onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
-@onready var pointer_icon := $Path2D/PathFollow2D/Pointer
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -18,19 +16,3 @@ func _input(event):
 		var delta_motion = (event.position - drag_start_pos) / zoom
 		position -= delta_motion
 		drag_start_pos = event.position
-
-func _process(delta: float) -> void:
-	update_pointer_icon()
-
-func update_pointer_icon():
-	var screen_size = get_viewport_rect().size / 2.7
-	var relative_position = to_local(player.global_position)
-	
-	if abs(relative_position.x) > screen_size.x or abs(relative_position.y) > screen_size.y && priority == 1:
-		path_follow.visible = true
-		var angle = global_position.angle_to_point(player.global_position)
-		var progress_ratio = (angle + PI) / TAU
-		path_follow.progress_ratio = progress_ratio
-		pointer_icon.rotation = angle + 3 * PI / 4
-	else:
-		path_follow.visible = false
