bc3745179713abf228bce04fd221320a354d7a56
Clément <146324049+ClementMONDARY@users.noreply.github.com>
2024-12-07 19:17:52 +0100

    PC Udpate

--- inventory/Item.gd
+++
@@ -1,8 +0,0 @@
-class_name Item
-extends Resource
-
-@export var id: String
-@export var name: String
-@export var icon: Texture2D
-@export var is_stackable: bool = false
-@export var max_stack: int = 1
